a16d9593d46267f70ce9dec273a427315c9b6300
Anthony La <anthonyyla.dev@gmail.com>
2025-07-10 19:47:04 -0700

    fix(velopack): Fixed not calling Velopack initialization.

--- src/Nagi/App.xaml.cs
+++ src/Nagi/App.xaml.cs
@@ -55,6 +55,7 @@ public partial class App : Application {
         InitializeComponent();
         UnhandledException += OnAppUnhandledException;
         CoreApplication.Suspending += OnSuspending;
+        VelopackApp.Build().Run();
     }
 
     /// <summary>
