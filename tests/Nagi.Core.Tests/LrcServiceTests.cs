0a61904e5a175b4796c0309e9d7ce72e9c55cc29
Anthony La <anthonyyla.dev@gmail.com>
2025-12-19 23:57:00 -0800

    Revert "fix(binding): moved binding to bind expressions"
    This reverts commit 6defbd454ddb350e4feb4439194c05a8c55bdb60.

--- tests/Nagi.Core.Tests/LrcServiceTests.cs
+++ tests/Nagi.Core.Tests/LrcServiceTests.cs
@@ -1,7 +1,5 @@
 ﻿using FluentAssertions;
 using Microsoft.Extensions.Logging;
-using Nagi.Core.Data;
-using Nagi.Core.Helpers;
 using Nagi.Core.Models;
 using Nagi.Core.Models.Lyrics;
 using Nagi.Core.Services.Abstractions;
@@ -20,28 +18,14 @@ public class LrcServiceTests
 {
     private const string LrcPath = "C:\\lyrics\\test.lrc";
     private readonly IFileSystemService _fileSystem;
-    private readonly IOnlineLyricsService _onlineLyricsService;
-    private readonly ISettingsService _settingsService;
-    private readonly IPathConfiguration _pathConfig;
-    private readonly ILibraryWriter _libraryWriter;
     private readonly ILogger<LrcService> _logger;
     private readonly LrcService _lrcService;
 
     public LrcServiceTests()
     {
         _fileSystem = Substitute.For<IFileSystemService>();
-        _onlineLyricsService = Substitute.For<IOnlineLyricsService>();
-        _settingsService = Substitute.For<ISettingsService>();
-        _pathConfig = Substitute.For<IPathConfiguration>();
-        _libraryWriter = Substitute.For<ILibraryWriter>();
         _logger = Substitute.For<ILogger<LrcService>>();
-        _lrcService = new LrcService(
-            _fileSystem,
-            _onlineLyricsService,
-            _settingsService,
-            _pathConfig,
-            _libraryWriter,
-            _logger);
+        _lrcService = new LrcService(_fileSystem, _logger);
     }
 
     #region GetLyricsAsync Tests
